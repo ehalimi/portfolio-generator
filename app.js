25c0e2abc22aafa446d0cbb124a9f4ff23640525
Ema Halimi <ehalimi@na.edu>
2022-07-20 21:03:12 -0500

    generated an HTML file

---
+++ app.js
@@ -0,0 +1,57 @@
+// var message = "Hello Node!";
+
+// var sum = 5 + 3;
+
+// console.log(message);
+// console.log(sum);
+
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+// var profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// const message = 'Hello Node';
+// message = 'Goodbye Node!';
+
+// const sum = 5 + 3;
+// sum += 1;
+
+// const animalArray = ['dog', 'cat', 'pig'];
+
+// animalArray.push('cow');
+
+// const personObj = {
+//     name: 'Ema',
+//     age: 22
+// };
+
+// personObj.age = 100;
+// personObj.occupation = 'Developer';
+
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('=============');
+
+//     profileDataArr.forEach(profileItem =>  console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
